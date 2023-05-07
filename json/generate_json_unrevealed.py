3798c7a97fb8871bf2848c135450776b7d1a58c4
H8 <94951726+h8ku@users.noreply.github.com>
2023-05-07 15:50:12 -0700

    Update generate_json_unrevealed.py

--- json/generate_json_unrevealed.py
+++ json/generate_json_unrevealed.py
@@ -1,10 +1,16 @@
 import json
+import os
 
 # Usage: python generate_json.py
-# Make sure you create an output folder first
-# You can edit the values below
+# You must edit the values below to fit your needs
+# Verify the output JSON files are how you want them to be
 
 cid_hash_image = "REPLACEME" # upload image with https://nft.storage/ then you can link to its cid here
+output_folder = "output"
+
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+    print(f'Created folder: "{output_folder}"')
 
 for x in range(1, 354+1):
     dictionary = {
@@ -14,5 +20,5 @@ for x in range(1, 354+1):
         "image": "ipfs://" + cid_hash_image + "/filename.png", # if gif or something else change its filename here
     }
     
-    with open("./output/" + str(x), "w") as outfile:
+    with open("./" + output_folder + "/" + str(x), "w") as outfile:
         json.dump(dictionary, outfile, indent=4)
