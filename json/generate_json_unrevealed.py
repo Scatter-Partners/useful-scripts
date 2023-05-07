5d2761523502b70689a41ca7c3eaac17a589af5b
H8 <94951726+h8ku@users.noreply.github.com>
2023-05-07 16:03:27 -0700

    Update generate_json_unrevealed.py

--- json/generate_json_unrevealed.py
+++ json/generate_json_unrevealed.py
@@ -2,23 +2,25 @@ import json
 import os
 
 # Usage: python generate_json.py
-# You must edit the values below to fit your needs
-# Verify the output JSON files are how you want them to be
+# Make sure you create an output folder first
+# You can edit the values below
 
-cid_hash_image = "REPLACEME" # upload image with https://nft.storage/ then you can link to its cid here
+cid_hash_image = "REPLACEME"  # upload image with https://nft.storage/ then you can link to its cid here
 output_folder = "output"
+image_filename = ""  # include this if needed
+supply = 1000
 
 if not os.path.exists(output_folder):
-    os.makedirs(output_folder)
-    print(f'Created folder: "{output_folder}"')
+  os.makedirs(output_folder)
+  print(f'Created folder: "{output_folder}"')
 
-for x in range(1, 354+1):
-    dictionary = {
-        "name": "Short name #" + str(x),
-        "description": "Description ",
-        "external_url": "https://scatter.art/",
-        "image": "ipfs://" + cid_hash_image + "/filename.png", # if gif or something else change its filename here
-    }
-    
-    with open("./" + output_folder + "/" + str(x), "w") as outfile:
-        json.dump(dictionary, outfile, indent=4)
+for x in range(1, supply + 1):
+  dictionary = {
+    "name": "Short name #" + str(x),
+    "description": "Description ",
+    "external_url": "https://scatter.art/",
+    "image": "ipfs://" + cid_hash_image + "/" + image_filename,
+  }
+
+  with open("./" + output_folder + "/" + str(x), "w") as outfile:
+    json.dump(dictionary, outfile, indent=4)
