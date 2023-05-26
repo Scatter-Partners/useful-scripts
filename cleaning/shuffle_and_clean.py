48bcb31b0a5b1851cca0ad614dd611e285d3ad87
h8ku <h8kucom@gmail.com>
2023-05-26 01:07:04 -0700

    Update shuffle_and_clean.py

--- cleaning/shuffle_and_clean.py
+++ cleaning/shuffle_and_clean.py
@@ -4,6 +4,9 @@ import json
 import random
 import shutil
 
+# Seed for randomness
+random.seed(1234) # change this number if you want a different random order
+
 # Directories
 input_dir = "input"
 output_dir = "output"
@@ -12,16 +15,19 @@ output_dir = "output"
 json_files = glob.glob(os.path.join(input_dir, "json", "*"))
 image_files = glob.glob(os.path.join(input_dir, "images", "*.png"))
 
+# Create dictionaries with basename as keys and full path as values
+json_files_dict = {os.path.basename(jf).split(".")[0]: jf for jf in json_files}
+image_files_dict = {os.path.basename(iff).split(".")[0]: iff for iff in image_files}
+
 # Make sure the number of JSON and image files are the same
-print(len(json_files), len(image_files))
-assert len(json_files) == len(image_files), "The number of JSON and image files should be the same"
+assert set(json_files_dict.keys()) == set(image_files_dict.keys()), "The JSON and image file names should match"
 
 # Create output directories if they don't exist
 os.makedirs(os.path.join(output_dir, "json"), exist_ok=True)
 os.makedirs(os.path.join(output_dir, "images"), exist_ok=True)
 
 # Create a list of pairs (json_file, image_file)
-pairs = list(zip(json_files, image_files))
+pairs = list(zip(json_files_dict.values(), image_files_dict.values()))
 random.shuffle(pairs)
 
 # Process the pairs
@@ -33,32 +39,12 @@ for i, (json_file, image_file) in enumerate(pairs, start=1):
     # Update JSON fields
     data["name"] = f"Name #{i}"
     data["image"] = f"ipfs://REPLACEME/{i}.png"
+    #data["description"] = "uncomment to add  description if you desire"
     
-    if "imageHash" in data:
-        del data["imageHash"]
-    if "dna" in data:
-        del data["dna"]
-    if "edition" in data:
-        del data["edition"]
-    if "date" in data:
-        del data["date"]
-    if "custom_fields" in data:
-        del data["custom_fields"]
-    if "file_url" in data: #not actually part of JSON format https://docs.opensea.io/docs/metadata-standards
-        del data["file_url"]
-    if "id" in data:
-        del data["id"]
-    if "seller_fee_basis_points" in data:
-        del data["seller_fee_basis_points"]
-    if "compiler" in data:
-        del data["compiler"]
-    if "properties" in data:
-        del data["properties"]
-    if "collection" in data:
-        del data["collection"]
-    if "external_url" in data:
-        del data["external_url"]           
-
+    for key in ["imageHash", "dna", "edition", "date", "custom_fields", "file_url", "id", 
+                "seller_fee_basis_points", "compiler", "properties", "collection", "external_url"]:
+        data.pop(key, None)
+            
     # Save the updated JSON
     with open(os.path.join(output_dir, "json", f"{i}"), "w", encoding="utf-8") as f:
         json.dump(data, f, indent=4)
